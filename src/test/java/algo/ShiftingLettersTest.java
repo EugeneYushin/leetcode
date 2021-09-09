30574d0d67f54ac70b93d79aa41fb490c6f77ee3
Yauheni Yushyn <yauheni.yushyn@datarobot.com>
2021-09-09 14:30:57 +0300

    rearrange tests

--- src/test/java/algo/ShiftingLettersTest.java
+++ src/test/java/algo/ShiftingLettersTest.java
@@ -2,7 +2,7 @@ package algo;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class ShiftingLettersTest {
 
@@ -14,7 +14,6 @@ class ShiftingLettersTest {
         assertEquals("a", cl.shiftingLetters("z", new int[]{1}));
         assertEquals("a", cl.shiftingLetters("a", new int[]{26}));
         assertEquals("rpl", cl.shiftingLetters("abc", new int[]{3, 5, 9}));
-        assertEquals("wqqwlcjnkphhsyvrkdod", cl.shiftingLetters("mkgfzkkuxownxvfvxasy", new int[]{505870226, 437526072, 266740649, 224336793, 532917782, 311122363, 567754492, 595798950, 81520022, 684110326, 137742843, 275267355, 856903962, 148291585, 919054234, 467541837, 622939912, 116899933, 983296461, 536563513}));
     }
 
     @Test
