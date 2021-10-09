1a818d848582e0c71b235351ef38c15592d64a4c
Yauheni Yushyn <yauheni.yushyn@datarobot.com>
2021-10-09 18:15:14 +0300

    add test for Combinations solution

--- src/test/java/algo/CombinationsTest.java
+++ src/test/java/algo/CombinationsTest.java
@@ -1,11 +1,28 @@
-//package algo;
-//
-//import org.junit.jupiter.api.Test;
-//
-//public class CombinationsTest {
-//    @Test
-//    void combine() {
-//        Combinations cl = new Combinations();
-//        assert(cl.combine(5, 3));
-//    }
-//}
+package algo;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CombinationsTest {
+    @Test
+    void combine() {
+        Combinations cl = new Combinations();
+        assertEquals(new ArrayList<List<Integer>>() {{
+            add(Arrays.asList(1, 2, 3));
+            add(Arrays.asList(1, 2, 4));
+            add(Arrays.asList(1, 2, 5));
+            add(Arrays.asList(1, 3, 4));
+            add(Arrays.asList(1, 3, 5));
+            add(Arrays.asList(1, 4, 5));
+            add(Arrays.asList(2, 3, 4));
+            add(Arrays.asList(2, 3, 5));
+            add(Arrays.asList(2, 4, 5));
+            add(Arrays.asList(3, 4, 5));
+        }}, cl.combine(5, 3));
+    }
+}
