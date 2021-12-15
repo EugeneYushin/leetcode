1e4001df09486cd83bdc30929eeca4102ff28384
Yauheni Yushyn <yauheni.yushyn@datarobot.com>
2021-12-15 12:16:46 +0300

    add optimized version for InsertionSortList

--- src/main/java/algo/InsertionSortList.java
+++ src/main/java/algo/InsertionSortList.java
@@ -2,10 +2,43 @@ package algo;
 
 public class InsertionSortList {
     /**
+     * Optimized for pre-sorted nodes
      * Time complexity: O(n^2)
      * Space complexity: O(1)
      */
     public ListNode insertionSortList(ListNode head) {
+        ListNode dummy = new ListNode();
+        dummy.next = head;
+        ListNode current = head;
+
+        while (current.next != null) {
+            // skip pre-sorted range
+            if (current.val <= current.next.val) {
+                current = current.next;
+                continue;
+            }
+
+            // we don't adjust `current` below, meaning we will process last element no matter while-loop condition
+            ListNode previous = dummy;
+            while (previous.next.val < current.next.val) {
+                previous = previous.next;
+            }
+
+            ListNode tmp = current.next;
+            current.next = current.next.next;
+
+            tmp.next = previous.next;
+            previous.next = tmp;
+        }
+
+        return dummy.next;
+    }
+
+    /**
+     * Time complexity: O(n^2)
+     * Space complexity: O(1)
+     */
+    public ListNode insertionSortList_classic(ListNode head) {
         ListNode dummy = new ListNode();
         ListNode current = head;
 
