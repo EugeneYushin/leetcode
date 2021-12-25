20b6302c6c4af5318ce423b4d89785113158e531
Yauheni Yushyn <yauheni.yushyn@datarobot.com>
2021-12-25 16:52:48 +0300

    add optimized version for BasicCalculator2

--- src/main/java/algo/BasicCalculator2.java
+++ src/main/java/algo/BasicCalculator2.java
@@ -8,9 +8,48 @@ import java.util.Stack;
 public class BasicCalculator2 {
     /**
      * Time complexity: O(n)
-     * Space complexity: O(n)
+     * Space complexity: O(1)
      */
     public int calculate(String s) {
+        int prevNum = 0;
+        int currentNum = 0;
+        char currentOp = '+';
+        int res = 0;
+
+        // 1+2+3*4-5
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+
+            if (Character.isDigit(c)) {
+                currentNum = currentNum * 10 + c - '0';
+            } // 1, 2, 3
+
+            if (!Character.isDigit(c) && c != ' ' || i == s.length() - 1) {
+                if (currentOp == '+') {
+                    res += prevNum; // 0+0, 0+1=1, 1+2
+                    prevNum = currentNum; //1,2,3
+                } else if (currentOp == '-') {
+                    res += prevNum;
+                    prevNum = -currentNum;
+                } else if (currentOp == '*') {
+                    prevNum *= currentNum;
+                } else {
+                    prevNum /= currentNum;
+                }
+
+                currentOp = c; // +, +
+                currentNum = 0;
+            }
+        }
+
+        return res + prevNum;
+    }
+
+    /**
+     * Time complexity: O(n)
+     * Space complexity: O(n)
+     */
+    public int calculate_stack(String s) {
         Stack<Integer> nums = new Stack<>();
         int currentNum = 0;
         char currentOp = '+';
